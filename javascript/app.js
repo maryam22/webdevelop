21cc368c0f16b94c2b4ace4059afc155fc107f76
maryam22 <maryambar22@gmail.com>
2021-05-07 10:24:48 +0200

    update

--- javascript/app.js
+++ javascript/app.js
@@ -16,6 +16,11 @@ function bruto(hour,  minute,  second){
 let thing = bruto(3,20,20);
 
 console.log("Bruto"+thing);
+/*
+console.log("I am ",myObject.name,myObject.Family,".born in",
+myObject.born,"I have lived in",myObject.LiveIn,"for",myObject.YearsIn,".I studied",
+myObject.study,"birthday datum",myObject.birthdayENMonth.day,"/",myObject.birthdayENMonth.month);*/
+//pass out side
 
 
 let myObject = {
@@ -33,8 +38,14 @@ let myObject = {
 
 }
 
-console.log("I am ",myObject.name,myObject.Family,".born in",
-myObject.born,"I have lived in",myObject.LiveIn,"for",myObject.YearsIn,".I studied",
-myObject.study,"birthday datum",myObject.birthdayENMonth.day,myObject.birthdayENMonth.month);
+function personInfo(person){
 
+   return (`Hello,I am ${person.name} ${person.Family}.born in ${person.born}. I have lived in ${person.LiveIn} for ${person.YearsIn}.I studied ${person.study}
+  . My birthday datum ${person.birthdayENMonth.day} / ${person.birthdayENMonth.month}`)
 
+}
+let print = personInfo(myObject);
+
+console.log(print);
+
+//pass inside
